e279fcd5af1cad4160de97ee8ff5600c7d3ae06c
Heri Setiawan <heri.dehero@gmail.com>
2019-08-16 08:24:42 +0700

    Update stylings

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ require('normalize.css/normalize.css');
 require('./styles/index.scss');
 
 const onMaintenance = () => alert('BCA Mobile sedang dalam maintenance.');
-document.getElementsByClassName('main-image')[0].onclick = onMaintenance;
+//document.getElementsByClassName('main-image')[0].onclick = onMaintenance;
